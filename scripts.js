f9e512ff6c8743941b6ba0487b790d77fcac1c03
Jacob DeCourcey <jakedecourcey@gmail.com>
2018-04-04 13:36:36 -0400

    removed header color change function

--- scripts.js
+++ scripts.js
@@ -1,6 +1,7 @@
 const MIN_RGB_VALUE = 0;
 const MAX_RGB_VALUE = 255;
 var targetColor;
+var colorSquares = document.querySelectorAll('.color-square');
 
 function getRandomRGBValue() {
   return Math.floor(Math.random() * ((MAX_RGB_VALUE+1) - MIN_RGB_VALUE) + MIN_RGB_VALUE);
@@ -27,7 +28,6 @@ function parseRGBColorToCSS (color) {
 }
 
 function randomizeColors() {
-  colorSquares = document.querySelectorAll('.color-square');
   for (let i = 0; i < colorSquares.length; i++) {
     colorSquares[i].style.background = parseRGBColorToCSS(getRandomRGBColor());
   }
@@ -42,8 +42,14 @@ function setTargetColor() {
   targetColor = getRandomRGBColor();
 }
 
+function setOneSquareToTargetColor() {
+  let chosenSquare = Math.floor(Math.random() * ((5+1) - 0) + 0); //Pick a number between 0 and 5 inclusive
+  colorSquares[chosenSquare].style.background = parseRGBColorToCSS(targetColor);
+}
+
 function resetBoard() {
   setTargetColor();
-  setHeaderToTargetColor();
   randomizeColors();
+  setOneSquareToTargetColor();
+  displayTargetColorInHeader();
 }
